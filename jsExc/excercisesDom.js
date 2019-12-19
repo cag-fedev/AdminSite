6e561a31c057ba41966df975b77e874d9bd0c9f7
maurizio.meluzzi <maurizio.meluzzi@accenture.com>
2019-12-19 15:31:20 +0100

    JS-HTML interactions (step 2)

--- jsExc/excercisesDom.js
+++ jsExc/excercisesDom.js
@@ -1,7 +1,7 @@
 
 let element01 = document.getElementById('jsTestBtn01');
 let element02 = document.getElementById('jsTestBtn02');
-let element03 = document.getElementById('jsTestBtn02');
+let element03 = document.getElementById('jsTestBtn03');
 
 element01.onclick = function() {
     console.log('Button 01 has been clicked.');
@@ -14,3 +14,9 @@ function btn02ClickMgr() {
     return false;
 };
 
+element03.addEventListener('click', function(event) { btn03ClickMgr(event) } )
+function btn03ClickMgr(cstmEv) {
+    cstmEv.preventDefault();
+    console.log('Button 03 has been clicked.');
+    return false;
+};
