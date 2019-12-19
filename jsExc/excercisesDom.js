f1edf05a08721056431d8ced5ea274c08b360857
maurizio.meluzzi <maurizio.meluzzi@accenture.com>
2019-12-19 16:26:10 +0100

    JS-HTML interactions (step 3)

--- jsExc/excercisesDom.js
+++ jsExc/excercisesDom.js
@@ -44,4 +44,26 @@ function anchor03ClickMgr(cstmEv) {
     return false;
 };
 
+document.onkeydown = function (event) {
+    event = event || window.event;
+    switch (event.key) {
+        case 'ArrowLeft':
+            console.log('left');
+            break;
 
+        case 'ArrowUp':
+            console.log('up');
+            break;
+
+        case 'ArrowRight':
+            console.log('right');
+            break;
+
+        case 'ArrowDown':
+            console.log('down');
+            break;
+
+        default: return;
+    }
+    event.preventDefault();
+};
