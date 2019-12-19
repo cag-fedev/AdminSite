133c5cacc912cad7d7cf1bef1836771053389ad8
maurizio.meluzzi <maurizio.meluzzi@accenture.com>
2019-12-19 16:59:24 +0100

    JS-HTML interactions (step 4)

--- jsExc/excercisesDom.js
+++ jsExc/excercisesDom.js
@@ -46,24 +46,56 @@ function anchor03ClickMgr(cstmEv) {
 
 document.onkeydown = function (event) {
     event = event || window.event;
+    let pacman = document.getElementById('pacman');
     switch (event.key) {
         case 'ArrowLeft':
             console.log('left');
+            moveLeft(pacman);
             break;
 
         case 'ArrowUp':
             console.log('up');
+            moveUp(pacman);
             break;
 
         case 'ArrowRight':
             console.log('right');
+            moveRight(pacman);
             break;
 
         case 'ArrowDown':
             console.log('down');
+            moveDown(pacman);
             break;
 
         default: return;
     }
     event.preventDefault();
 };
+
+function moveDown(element) {
+    var elStyle = window.getComputedStyle(element);
+    var topValue = elStyle.getPropertyValue("top").replace("px", "");
+    element.style.top = (Number(topValue) + 20) + "px";
+}
+
+function moveUp(element) {
+    var elStyle = window.getComputedStyle(element);
+    var topValue = elStyle.getPropertyValue("top").replace("px", "");
+    element.style.top = (Number(topValue) - 20) + "px";
+}
+
+function moveRight(element) {
+    var elStyle = window.getComputedStyle(element);
+    var leftValue = elStyle.getPropertyValue("left").replace("px", "");
+    element.style.left = (Number(leftValue) + 20) + "px";
+}
+
+function moveLeft(element) {
+    var elStyle = window.getComputedStyle(element);
+    var leftValue = elStyle.getPropertyValue("left").replace("px", "");
+    element.style.left = (Number(leftValue) - 20) + "px";
+}
+
+
+
