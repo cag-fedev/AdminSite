501901e7b26e955a8019c66c7aec004bb10c32cb
Pani <claudia.pani@accenture.com>
2019-12-19 16:21:07 +0100

    Claudia html js

--- jsExc/excercisesDom.js
+++ jsExc/excercisesDom.js
@@ -3,7 +3,7 @@ let element01 = document.getElementById('jsTestBtn01');
 let element02 = document.getElementById('jsTestBtn02');
 let element03 = document.getElementById('jsTestBtn03');
 
-element01.onclick = function() {
+element01.onclick = function () {
     console.log('Button 01 has been clicked.');
     return false;
 };
@@ -14,9 +14,34 @@ function btn02ClickMgr() {
     return false;
 };
 
-element03.addEventListener('click', function(event) { btn03ClickMgr(event) } )
+element03.addEventListener('click', function (event) { btn03ClickMgr(event) })
 function btn03ClickMgr(cstmEv) {
     cstmEv.preventDefault();
     console.log('Button 03 has been clicked.');
     return false;
 };
+
+//Ancore
+let element04 = document.getElementById('Anchor01');
+let element05 = document.getElementById('Anchor02');
+let element06 = document.getElementById('Anchor03');
+
+element04.onclick = function () {
+    console.log('Anchor 01 has been clicked.');
+    //return false;
+};
+
+element05.addEventListener('click', anchor02ClickMgr)
+function anchor02ClickMgr() {
+    console.log('Anchor 02 has been clicked.');
+    return false;
+};
+
+element06.addEventListener('click', function (event) { anchor03ClickMgr(event) })
+function anchor03ClickMgr(cstmEv) {
+    cstmEv.preventDefault();
+    console.log('Anchor 03 has been clicked.');
+    return false;
+};
+
+
