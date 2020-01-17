12ccb0895ec0f155e623c83af484a14c577971de
maurizio-meluzzi-accenture <58983999+maurizio-meluzzi-accenture@users.noreply.github.com>
2020-01-17 16:03:52 +0100

    MM minor JS changes

--- jsExc/excercisesDom-MM.js
+++ jsExc/excercisesDom-MM.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", executeAfterPageLoads);
+let jsTestBtn01 = document.getElementById('jsTestBtn01');
+let currentColor = 'red';
 
 function executeAfterPageLoads()
 {
     console.log('general function is started');
+    // setTimeout(functionCalledAfterTenSeconds, 10000);
+    // setInterval(functionCalledEachFiveSeconds, 5000);
+    setInterval(switchColor, 3000);
 }
 
+function functionCalledAfterTenSeconds()
+{
+    console.log('Ten seconds have passed');
+}
+
+function functionCalledEachFiveSeconds()
+{
+    console.log('Five seconds have passed');
+}
+
+function switchColor()
+{
+    if (currentColor === 'red')  currentColor = 'green';
+    else                         currentColor = 'red';
+    jsTestBtn01.style.backgroundColor = currentColor;
+}
 
